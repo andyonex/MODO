399d37fae089ba994b68c9e4351940fba9c953d3
andyonex <32534818+andyonex@users.noreply.github.com>
2017-10-21 20:26:13 +0300

    Create renameOneByOne.py
    Script for MODO

---
+++ Scripts/renameOneByOne.py
@@ -0,0 +1,27 @@
+# python
+# Rename one by one
+# Author: Andy Budanov
+# Use to rename multiple objects. 
+# It will turn off visibility for not selected and set the viewport on currently renaming object
+# Usage: select multiple objects and run @renameOneByOne.py 
+# ```python
+
+import lx
+import modo
+scene = modo.Scene()
+
+# store init state
+isIsolated = lx.eval("view3d.inactiveInvisible ?")
+
+lx.eval("view3d.inactiveInvisible true")
+
+# get selected items
+selectedMeshes = scene.selected
+
+for meshItem in selectedMeshes:
+	scene.select(meshItem)
+
+	lx.eval("viewport.fitSelected")	
+	lx.eval("layer.renameSelected")
+
+lx.eval("view3d.inactiveInvisible %s" %isIsolated)
