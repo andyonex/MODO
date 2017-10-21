5de3170b8828194133c91eb018b6db9a60be620a
andyonex <32534818+andyonex@users.noreply.github.com>
2017-10-21 20:28:55 +0300

    Add files via upload
    New MODO script

---
+++ Scripts/placeInstances.py
@@ -0,0 +1,32 @@
+# python
+# Place instances to other objects position
+# Author: Andy Budanov
+# Place an instance of the last selected object to match to transformation of every other selected object
+# Usage: 
+# 1. Select multiple objects you want to replace.
+# 2. Select object that you want to instantiate 
+# 3. run the script with @placeInstances.py
+# you'll be left with replaced objects selection (without the last one) so you can delete or group them
+# ```python
+
+import lx
+import modo
+scene = modo.Scene()
+
+selectedMeshes = scene.selected
+
+if len( selectedMeshes ) > 2:
+	org = selectedMeshes[len(selectedMeshes)-1]
+	for item in scene.selected:
+		if item.id != org.id:
+			instance = modo.Scene().duplicateItem(org, instance=True)
+			transf = modo.LocatorSuperType(item).position.get()
+			rot =  modo.LocatorSuperType(item).rotation.get()
+			scl =  modo.LocatorSuperType(item).scale.get()
+			modo.LocatorSuperType(instance).position.set(transf)
+			modo.LocatorSuperType(instance).rotation.set(rot)
+			modo.LocatorSuperType(instance).scale.set(scl)
+
+# remove last element from selection
+selectedMeshes.pop()
+scene.select(selectedMeshes)
